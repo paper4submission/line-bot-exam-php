937e957e697f655749804fab6270701e1e51be5b
paper4submission <51797456+paper4submission@users.noreply.github.com>
2019-06-15 09:23:23 +0700

    Update botpush.php

--- botpush.php
+++ botpush.php
@@ -4,9 +4,10 @@
 
 require "vendor/autoload.php";
 
-$access_token = 'giVAN+sxs022hITq9CZn3YymXam9udcCjz6XyiPX5QiMOIkl/21EiyLXZZFlLZZJUm1kih/MIG15yzac79ZH+WDapHOYUTt0/aljgkpDScxZrAIlHDZT9EDXCjUMzQNI9NueDgojxEVopdQwInfoRwdB04t89/1O/w1cDnyilFU=';
+// for App name : EENET2019_MSG
+$access_token = '/uI+6i00zwm0cpsR2sS6FL5ywLmrj5Bpb/xoG5Yfw4/dJGXbsablO30bet7BByR8lp30Xx0hgrKpoxGEEMwNobIlzQO8DGEpaWLvTNXCILXsxj5hw14+ZYAy1II0lFI3tp3xGelMW7hQuxM2GvzC/AdB04t89/1O/w1cDnyilFU=';
 
-$channelSecret = '2840d52f8356a455e0905b9639f0cf8d';
+$channelSecret = '1ad759c60ccdc9b2a4d3ac4addad3d87';
 
 $pushID = 'U8b25b0c27039892189efade980b8749a';
 
