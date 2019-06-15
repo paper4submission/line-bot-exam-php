22b4c052a8bb5ec195486a98a7f614034a247bf8
paper4submission <51797456+paper4submission@users.noreply.github.com>
2019-06-15 10:28:44 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -4,9 +4,13 @@ require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
 // App Name : EENET2020_MSG
-$access_token = 'nfaeu7GfZamJ+c0pgsj9iB9EPM8HSNQQzT7i1FwsVnKRTokM6GWPD6x5RdjdbFrqpajVQIbr+HJYalqdT/Bw4ffKlN9I4glLNp8VNuPn8a8fB1TZxG7IcDyzs9geSfdZMrgUtfzJhkH3y2y5Vr7zxQdB04t89/1O/w1cDnyilFU=';
+// $access_token = 'nfaeu7GfZamJ+c0pgsj9iB9EPM8HSNQQzT7i1FwsVnKRTokM6GWPD6x5RdjdbFrqpajVQIbr+HJYalqdT/Bw4ffKlN9I4glLNp8VNuPn8a8fB1TZxG7IcDyzs9geSfdZMrgUtfzJhkH3y2y5Vr7zxQdB04t89/1O/w1cDnyilFU=';
 
-// Get POST body content
+// App Name : Test_2020
+$access_token = 'giVAN+sxs022hITq9CZn3YymXam9udcCjz6XyiPX5QiMOIkl/21EiyLXZZFlLZZJUm1kih/MIG15yzac79ZH+WDapHOYUTt0/aljgkpDScxZrAIlHDZT9EDXCjUMzQNI9NueDgojxEVopdQwInfoRwdB04t89/1O/w1cDnyilFU='
+
+	
+	// Get POST body content
 $content = file_get_contents('php://input');
 // Parse JSON
 $events = json_decode($content, true);
